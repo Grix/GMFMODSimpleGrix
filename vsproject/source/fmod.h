7a5b2b72cdc4a15080e3f42e6d81eee34f772179
Gitle Mikkelsen <gitlem@gmail.com>
2015-09-18 16:10:14 +0200

    msvcr120

--- vsproject/source/fmod.h
+++ vsproject/source/fmod.h
@@ -15,7 +15,7 @@
     0xaaaabbcc -> aaaa = major version number.  bb = minor version number.  cc = development version number.
 */
 
-#define FMOD_VERSION    0x00044454
+#define FMOD_VERSION    0x00044456
 
 /*
     Compiler specific settings.
